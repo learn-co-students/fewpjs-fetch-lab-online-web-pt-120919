4bd9bf07af0c60dfde24feb1fe44433a249b5bf9
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-07-01 17:58:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function fetchBooks("https://anapioficeandfire.com/api/books").then(renderBooks(response) {
-  return Response.json();
-
-})
+function fetchBooks() {
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json =>  renderBooks(json))
+}
 
 function renderBooks(books) {
   const main = document.querySelector('main')

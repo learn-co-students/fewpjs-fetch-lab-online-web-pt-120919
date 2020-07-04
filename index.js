097c805a71a4192584a5fcf40af5e7e345887d9f
Mely07 <damely.tineo07@gmail.com>
2020-07-04 17:06:42 -0400

    Done.

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ function fetchBooks() {
 
 function renderBooks(books) {
   const main = document.querySelector('main')
-  books.forEach(book => {
+  
+  for (let book of books){
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
-  })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
